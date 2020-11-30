b363a778dd3ae4b4613439f574ed177c20c12007
odellerh <1700305@sit.singaporetech.edu.sg>
2020-12-01 02:18:38 +0800

    closed bracket

--- src/crypto/zinc/chacha20/chacha20.c
+++ src/crypto/zinc/chacha20/chacha20.c
@@ -72,7 +72,7 @@ static inline bool hchacha20_arch(u32 derived_key[CHACHA20_KEY_WORDS],
 	DOUBLE_ROUND(x), \
 	DOUBLE_ROUND(x), \
 	DOUBLE_ROUND(x), \
-	DOUBLE_ROUND(x)/*, \
+	DOUBLE_ROUND(x)\/*, \
 	DOUBLE_ROUND(x), \
 	DOUBLE_ROUND(x), \
 	DOUBLE_ROUND(x), \
